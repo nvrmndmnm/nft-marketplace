e9b9825e8fb36de9d79d27c87295682341f44467
Said <s.avkhadeev@gmail.com>
2022-03-15 08:22:04 +0600

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,30 @@
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+import "solidity-coverage";
+import "hardhat-contract-sizer";
+import 'dotenv/config';
+
+module.exports = {
+  solidity: "0.8.1",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.WALLET_PRIVATE_KEY, process.env.WALLET2_PRIVATE_KEY],
+      gasLimit: 80000000
+    },
+    localhost: {
+      url: 'http://localhost:8545'
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  contractSizer: {
+    alphaSort: true,
+    disambiguatePaths: false,
+    runOnCompile: true,
+    strict: true,
+    only: [':ERC20$'],
+  }
+};
